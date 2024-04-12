3a9ee3c3d7882172a3316115f8ede0df20497ea3
Yevhen Zakharevych <yzakharevych@fieldcomplete.com>
2024-04-12 11:46:16 +0300

    homework

---
+++ js/task-2.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * @param {Array<String>} firstArray
+ * @param {Array<String>} secondArray
+ * @param {number} maxLength
+ */
+function makeArray(firstArray, secondArray, maxLength) {
+  const array = [...firstArray, ...secondArray];
+
+  if (array.length > maxLength) {
+    return array.slice(0, maxLength);
+  }
+
+  return array;
+}
+
+console.log('--- Task 2 ---');
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
